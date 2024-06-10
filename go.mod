0ffba1f6537a5abc080e5f1a86997db7a7804d98
eric ng <naclericny@gmail.com>
2024-06-09 22:49:20 -0400

    first commit

---
+++ go.mod
@@ -0,0 +1,21 @@
+module collision
+
+go 1.21.1
+
+require (
+	github.com/ebitengine/purego v0.6.0 // indirect
+	github.com/go-gl/glfw/v3.3/glfw v0.0.0-20200707082815-5321531c36a2 // indirect
+	github.com/hajimehoshi/ebiten v1.12.12 // indirect
+	github.com/hajimehoshi/ebiten/v2 v2.6.6 // indirect
+	github.com/jezek/xgb v1.1.0 // indirect
+	github.com/solarlune/ldtkgo v0.9.3 // indirect
+	github.com/tidwall/gjson v1.6.4 // indirect
+	github.com/tidwall/match v1.0.1 // indirect
+	github.com/tidwall/pretty v1.0.2 // indirect
+	github.com/yuin/goldmark v1.7.0 // indirect
+	golang.org/x/exp/shiny v0.0.0-20230817173708-d852ddb80c63 // indirect
+	golang.org/x/image v0.12.0 // indirect
+	golang.org/x/mobile v0.0.0-20230922142353-e2f452493d57 // indirect
+	golang.org/x/sync v0.3.0 // indirect
+	golang.org/x/sys v0.12.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	"image"
+	_ "image/png"
+	"log"
+
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"github.com/hajimehoshi/ebiten/v2/inpututil"
+	"github.com/solarlune/ldtkgo"
+)
+
+var (
+	game        *Game
+	ldtkProject *ldtkgo.Project
+	floorSprite *ebiten.Image
+)
+
+const (
+	tileSize     = 16
+	screenWidth  = 256
+	screenHeight = 128
+
+	// Map Dimension 16 by 8 tiles
+	cameraWidth          = 16
+	cameraHeight         = 8
+	cameraScale  float64 = float64(screenWidth) / tileSize / float64(cameraWidth)
+)
+
+type Camera struct {
+	pX float64
+	pY float64
+}
+
+type Game struct {
+	keys   []ebiten.Key
+	camera Camera
+}
+
+func (g *Game) Update() error {
+	g.keys = inpututil.AppendPressedKeys(g.keys[:0])
+
+	if inpututil.IsKeyJustPressed(ebiten.KeyQ) {
+		panic("Game quit change this later")
+	}
+	return nil
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+	ebitenutil.DebugPrintAt(screen, "Q to quit", 0, 0)
+	ebitenutil.DebugPrintAt(screen, "Arrow Keys to move Camera", 0, 16)
+
+	var cameraOffsetX float64
+	var cameraOffsetY float64
+
+	cameraOffsetX = float64(g.camera.pX) * 16 * -1
+	cameraOffsetY = float64(g.camera.pY) * 16 * -1
+
+	for _, layer := range ldtkProject.Levels[0].Layers {
+		for _, tile := range layer.Tiles {
+			op := &ebiten.DrawImageOptions{}
+			op.GeoM.Translate(float64(tile.Position[0])+cameraOffsetX, float64(tile.Position[1])+cameraOffsetY)
+			op.GeoM.Scale(cameraScale, cameraScale)
+			screen.DrawImage(floorSprite.SubImage(image.Rect(tile.Src[0], tile.Src[1], tile.Src[0]+16, tile.Src[1]+16)).(*ebiten.Image), op)
+		}
+	}
+
+	for _, keyPress := range g.keys {
+		switch keyPress {
+		case ebiten.KeyUp:
+			g.camera.pY += -.25
+		case ebiten.KeyDown:
+			g.camera.pY += .25
+		case ebiten.KeyLeft:
+			g.camera.pX += -.25
+		case ebiten.KeyRight:
+			g.camera.pX += .25
+		default:
+		}
+	}
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
+	return screenWidth, screenHeight
+}
+
+func init() {
+	game = &Game{camera: Camera{0, 0}}
+	var err error
+	ldtkProject, err = ldtkgo.Open("assets/demo/demo.ldtk")
+	if err != nil {
+		panic("Map file doesn't exist")
+	}
+
+	floorSprite, _, err = ebitenutil.NewImageFromFile("./assets/demo/floor.png")
+	if err != nil {
+		panic("Tilemap doesn't exist")
+	}
+
+}
+
+func main() {
+	ebiten.SetWindowSize(screenWidth*2, screenHeight*2)
+	ebiten.SetWindowTitle("Platformer")
+	// ebiten.SetFullscreen(true)
+
+	if err := ebiten.RunGame(game); err != nil {
+		log.Fatal(err)
+	}
+
+}
