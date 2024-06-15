90aa58d2b9a7a6f2d8703531570ea335f822e583
Eric Ng <ericng@Erics-MacBook-Pro.local>
2024-06-15 02:09:47 -0400

    wip

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"image"
 	"image/color"
 	_ "image/png"
 	"log"
@@ -73,31 +72,49 @@ func (g *Game) Update() error {
 	return nil
 }
 
-/*
-func (p *Player) IdleAnimation(screen *ebiten.Image, offSetX, offSetY float64) {
-	op := &ebiten.DrawImageOptions{}
-	if p.direction == "LEFT" {
-		op.GeoM.Scale(cameraScale*-1, cameraScale)
-		op.GeoM.Translate(float64(p.idleAnim.sc.frameWidth)*cameraScale, 0)
-	} else if p.direction == "RIGHT" {
-		op.GeoM.Scale(cameraScale, cameraScale)
+type SpriteCell struct {
+	cellX       int // column of spritesheet Ex: 0 is first col 16 is 2nd col
+	cellY       int // row of spritesheet Ex: 16 is 2nd row
+	frameWidth  int // Size of Sprite frame (most likely 16x16)
+	frameHeight int
+}
+
+func (sc *SpriteCell) getRow(cellY int) int {
+	return cellY * sc.frameHeight
+}
+
+func (sc *SpriteCell) getCol(cellX int) int {
+	return cellX * sc.frameWidth
+}
+
+type AnimationData struct {
+	sc             SpriteCell
+	frameCount     int // Total number of columns for specific row
+	frameFrequency int // How often frames transition
+}
+
+type Unit struct {
+	posX     float32
+	posY     float32
+	width    int
+	height   int
+	idleAnim AnimationData
+}
+
+func CreateUnit(spritesheet *ebiten.Image) Unit {
+	idleAnimData := AnimationData{SpriteCell{0, 0, 16, 16}, 2, 64}
+
+	u := Unit{
+		posX:     0,
+		posY:     0,
+		width:    16,
+		height:   16,
+		idleAnim: idleAnimData,
 	}
 
-	op.GeoM.Translate(p.posX+offSetX, p.posY+offSetY)
-
-	cellX := p.idleAnim.sc.cellX
-	cellY := p.idleAnim.sc.cellY
-
-	i := (game.count / p.idleAnim.frameFrequency) % p.idleAnim.frameCount
-	sx, sy := p.idleAnim.sc.getCol(cellX)+i*p.idleAnim.sc.frameWidth, p.idleAnim.sc.getRow(cellY)
-	screen.DrawImage(p.spritesheet.SubImage(image.Rect(sx, sy, sx+p.idleAnim.sc.frameWidth, sy+p.idleAnim.sc.frameHeight)).(*ebiten.Image), op)
+	return u
 }
-*/
-/*
-type Unit struct {
-	gridCoord
-}
-*/
+
 func (g *Game) Draw(screen *ebiten.Image) {
 	ebitenutil.DebugPrintAt(screen, "Q to quit", 0, 0)
 	ebitenutil.DebugPrintAt(screen, "Arrow Keys to move Camera", 0, 16)
@@ -109,16 +126,14 @@ func (g *Game) Draw(screen *ebiten.Image) {
 	cameraOffsetY = g.camera.pY * 16 * -1
 
 	renderGrid(screen, g.grid, cameraOffsetX, cameraOffsetY)
-	screen.DrawImage(floorSprite.SubImage(image.Rect(tile.Src[0], tile.Src[1], tile.Src[0]+16, tile.Src[1]+16)).(*ebiten.Image), op)
+
 	/*
-		for _, layer := range ldtkProject.Levels[0].Layers {
-			for _, tile := range layer.Tiles {
-				op := &ebiten.DrawImageOptions{}
-				op.GeoM.Translate(float64(tile.Position[0])+cameraOffsetX, float64(tile.Position[1])+cameraOffsetY)
-				op.GeoM.Scale(cameraScale, cameraScale)
-				screen.DrawImage(floorSprite.SubImage(image.Rect(tile.Src[0], tile.Src[1], tile.Src[0]+16, tile.Src[1]+16)).(*ebiten.Image), op)
-			}
-		}
+		cellX := p.idleAnim.sc.cellX
+		cellY := p.idleAnim.sc.cellY
+
+		i := (game.count / p.idleAnim.frameFrequency) % p.idleAnim.frameCount
+		sx, sy := p.idleAnim.sc.getCol(cellX)+i*p.idleAnim.sc.frameWidth, p.idleAnim.sc.getRow(cellY)
+		screen.DrawImage(p.spritesheet.SubImage(image.Rect(sx, sy, sx+p.idleAnim.sc.frameWidth, sy+p.idleAnim.sc.frameHeight)).(*ebiten.Image), op)
 	*/
 
 	for _, keyPress := range g.keys {
@@ -143,7 +158,7 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
 
 func LoadSpritesheets() {
 	var err error
-	unitSprite, _, err = ebitenutil.NewImageFromFile("./assets/Characters/chicken_sprites.png")
+	unitSprite, _, err = ebitenutil.NewImageFromFile("./assets/demo/eliwood_map_idle.png")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -163,6 +178,7 @@ func init() {
 	}
 
 	LoadSpritesheets()
+	CreateUnit(unitSprite)
 
 }
 
